dfb8ef5f8af217341a64ddadcc74beb14b821204
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-23 19:53:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["wow", "I", "American Football", "arrays!"]
+  ["wow", "I", "am", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array,index_number)
-  create_an_array.index[wow]
+  create_an_array.index[2]
 end
 
 def retrieve_first_element_from_array(array)
