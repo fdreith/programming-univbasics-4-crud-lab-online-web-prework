b6cc762145de5242f983ea2c75bc8a0a9a2451bb
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-23 19:30:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["The Acorn", "The Dodos", "American Football", "arrays!"]
+  ["wow", "The Dodos", "American Football", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
